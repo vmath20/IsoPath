417506c87f36ce8a0df1cc198ddec61771afa933
Vaibhav Mishra <vaibhavrichmishra@gmail.com>
2025-04-30 15:13:35 -0400

    Update plip.py

--- generate_embeddings/plip.py
+++ generate_embeddings/plip.py
@@ -3,12 +3,8 @@ import os
 import numpy as np
 import torch
 import pandas as pd
-from skimage import filters, color
 from math import ceil
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-from torchvision import transforms
-import timm
 from huggingface_hub import login
 
 metadata_path = "/tcga/open-access/gdc_data_portal/biospecimen/tcga_Biospecimen_SAMPLE_METADATA/2023-09-01/gdc_sample_sheet.2023-09-05.tsv"
