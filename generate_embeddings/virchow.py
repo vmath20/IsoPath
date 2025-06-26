7d4221880201427c3d3974c48a2c06745d9e94c3
Vaibhav Mishra <vaibhavrichmishra@gmail.com>
2025-06-26 16:17:36 -0400

    Update virchow.py

--- generate_embeddings/virchow.py
+++ generate_embeddings/virchow.py
@@ -11,12 +11,24 @@ from timm.layers import SwiGLUPacked
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 
+#Constants
+num_slides = 250
+num_patches_per_slide = 250
+patch_size = 224
+
 metadata_path = "/tcga/open-access/gdc_data_portal/biospecimen/tcga_Biospecimen_SAMPLE_METADATA/2023-09-01/gdc_sample_sheet.2023-09-05.tsv"
 metadata_df = pd.read_csv(metadata_path, sep='\t')
 slides_df = metadata_df[metadata_df['Data Type'] == 'Slide Image']
 slides_df = slides_df.sort_values(by='Project ID').reset_index(drop=True)
 base_dir = '/tcga/open-access/gdc_data_portal/biospecimen/tcga_Biospecimen_FILES/'
 slides_df['Full Path'] = slides_df.apply(lambda row: os.path.join(base_dir, row['File ID'], row['File Name']), axis=1)
+
+# Load preprocessed patches
+preprocessed_patches_dir_brca = "preprocessed_patches_BRCA"
+preprocessed_patches_dir_luad = "/lotterlab/users/vmishra/preprocessed_patches_LUAD"
+preprocessed_patches_dir_lusc = "/lotterlab/users/vmishra/preprocessed_patches_LUSC"
+preprocessed_patches_dir_coad = "/lotterlab/users/vmishra/preprocessed_patches_COAD"
+login(token = "YOUR_HF_TOKEN") # Replace with hugging face token
 
 def embed(
     patches,
@@ -59,17 +71,11 @@ def embed(
 
     return opt_embs
 
-num_slides = 250
-num_patches_per_slide = 250
-patch_size = 224
-
 brca_embeddings = []
 luad_embeddings = []
 lusc_embeddings = []
 coad_embeddings = []
 
-# Replace line below with hugging face token
-login(token = "YOUR_HF_TOKEN")
 model = timm.create_model(
     "hf-hub:paige-ai/Virchow2",
     pretrained=True,
@@ -79,12 +85,6 @@ model = timm.create_model(
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 model = model.eval()
 preprocess = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
-
-# Load preprocessed patches
-preprocessed_patches_dir_brca = "preprocessed_patches_BRCA"
-preprocessed_patches_dir_luad = "/lotterlab/users/vmishra/preprocessed_patches_LUAD"
-preprocessed_patches_dir_lusc = "/lotterlab/users/vmishra/preprocessed_patches_LUSC"
-preprocessed_patches_dir_coad = "/lotterlab/users/vmishra/preprocessed_patches_COAD"
 
 def load_patches_brca(category_label):
     patches_list = []
